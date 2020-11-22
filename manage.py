d09194c28056dcb3465e1624db4a4a2b4723574c
amtesire <tesiregisele@gmail.com>
2020-11-22 20:26:40 +0200

    added changes

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app
 from flask_script import Manager, Server
 
-app= create_app('development')
+app = create_app('development')
 
 manager= Manager(app)
 manager.add_command('server',Server)
@@ -12,5 +12,5 @@ def test():
     import unittest
     tests =unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-if __name__=='__main__':
+if __name__=="__main__":
     manager.run()
